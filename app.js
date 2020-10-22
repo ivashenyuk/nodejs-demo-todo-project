1ee5c2fdd9516cf4062a687f996b7274e95b261a
Yuriy Ivashenyuk <yuriy@solacein.com>
2020-10-22 22:54:09 +0300

    created registration and login route

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const app = express()
+const auth = require('./src/auth')
+require('dotenv').config();
+
+const bodyParser = require('body-parser');
+const expressSession = require('express-session')({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressSession);
+
+
+
+
+//  Use auth routes  
+auth(app);
+
 const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`App listening at http://localhost:${port}`)
 })
---
+++ src/mongo/entities/user.js
@@ -0,0 +1,57 @@
+const passport = require('passport');
+const mongoose = require('./../mongoose-connection');
+const { Schema } = mongoose;
+const bcrypt = require('bcrypt');
+const UserSchema = new Schema({
+    email: String,
+    password: String,
+    token: String,
+});
+
+// pre saving user
+UserSchema.pre('save', function (next) {
+    const user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (this.isModified('password') || this.isNew) {
+        bcrypt.genSalt(10, function (error, salt) {
+            // handle error
+            if (error) return next(error);
+
+            // hash the password using our new salt
+            bcrypt.hash(user.password, salt, function (error, hash) {
+                // handle error
+                if (error) return next(error);
+
+                // override the cleartext password with the hashed one
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        return next();
+    }
+});
+
+UserSchema.methods.comparePassword = function(passw, cb) {
+    bcrypt.compare(passw, this.password, function(err, isMatch) {
+      if (err) {
+        return cb(err);
+      }
+      cb(null, isMatch);
+    });
+  };
+
+UserSchema.statics.isValid = function (req) {
+    if (req) {
+        const email = req.body.email || '';
+        const password = req.body.password || '';
+
+        if (email && password && String(password).length > 6) {
+            return true;
+        }
+    }
+    return false;
+}
+
+module.exports = mongoose.model('User', UserSchema);
